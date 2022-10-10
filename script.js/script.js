b4a9950de4f77d917a1cddf9a66174d0bbb7626a
Jonathan <Spircoff_j@yahoo.com>
2022-10-10 11:42:05 -0400

    Added remainer of rows with buttons and content

--- script.js/script.js
+++ script.js/script.js
@@ -1,16 +1,26 @@
 $(document).ready();
 //variables
 let currentTime = moment().format("MMMM Do YYYY, hh:mm:ss a");
-
+let hoursToday = [
+  "9am",
+  "10am",
+  "11am",
+  "12pm",
+  "1pm",
+  "2pm",
+  "3pm",
+  "4pm",
+  "5pm",
+];
 //Connected elements
 const timeRows = $(".container");
+const hours = $("#one");
 
 //Dynamic Date Function
 setInterval(function setTime() {
   currentTime = moment().format("MMMM Do YYYY");
   $("#currentDay").text(currentTime);
 }, 1000);
-
 
 // function appendRows() {
 //     for (let i = 0; i < 5; i++) {
